88ff46ee235eefd1fe62d24b42788c93d3172d9a
Arslan <arslan1.qau11@gmail.com>
2023-08-31 12:45:28 +0200

    validate form

--- pages/1_⚙️_Analyze.py
+++ pages/1_⚙️_Analyze.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 from streamlit_plotly_events import plotly_events
 import subprocess
@@ -41,7 +42,7 @@ if selected_mzML_file:
 if selected_fasta_file:
     database_file_path = str(Path(st.session_state.workspace, "fasta-files", selected_fasta_file))
 
-preset_list = ( 'none', 'RNA-UV (U)', 'RNA-UV(UCGA)', 'RNA-UV Extended (U)', 'RNA-UV Extended (UCGA)',
+preset_list = ( 'none', 'RNA-UV (U)', 'RNA-UV (UCGA)', 'RNA-UV Extended (U)', 'RNA-UV Extended (UCGA)',
     'RNA-UV (4SU)', 'RNA-UV Extended (4SU)', 'RNA-UV (6SG)',
     'RNA-UV Extended (6SG)', 'RNA-DEB', 'RNA-DEB Extended',
     'RNA-NM', 'RNA-NM Extended', 'DNA-UV', 'DNA-UV Extended',
@@ -65,24 +66,44 @@ with cols[0]:
         Enzyme = st.selectbox('Enzyme',Enzyme, help=" The enzyme used for peptide digestion. (default: 'Trypsin/P')")
     with cols_[1]:
         Missed_cleavages = str(st.number_input("Missed_cleavages",value=2, help="Number of missed cleavages. (default: '2')"))
+        if int(Missed_cleavages) <= 0:
+            st.error("Length must be a positive integer greater than 0.")
+
 with cols[1]:
     cols_=st.columns(2)
     with cols_[0]:
         peptide_min = st.text_input('peptide min length', '6', help="Minimum size a peptide must have after digestion to be considered in the search. (default: '6')")
+        if peptide_min != "":
+            if not peptide_min.isdigit():
+                st.error("Length must be a positive integer greater than 1.")
+            elif int(peptide_min) < 1:
+                st.error("Length must be a positive integer greater than 1.")
+
     with cols_[1]:
         peptide_max= st.text_input('peptide max length', '1000000', help="Maximum size a peptide may have after digestion to be considered in the search. (default: '1000000')")
+        if peptide_max != "":
+            if not peptide_max.isdigit():
+                st.error("Length must be a positive integer greater than 1.")
+            elif int(peptide_max) < 1:
+                st.error("Length must be a positive integer greater than 1.")
 
 cols=st.columns(2)
 with cols[0]:
     cols_=st.columns(2)
     with cols_[0]:
         Precursor_MT = str(st.number_input("Precursor mass tolerance",value=6, help = "Precursor mass tolerance (+/- around precursor m/z). (default: '6.0')"))
+        if int(Precursor_MT) <= 0:
+            st.error("Precursor mass tolerance must be a positive integer")
+
     with cols_[1]:
         Precursor_MT_unit= st.selectbox('Precursor mass tolerance unit',['ppm', 'Da'])
 with cols[1]:
     cols_=st.columns(2)
     with cols_[0]:
         Fragment_MT = str(st.number_input("Fragment mass tolerance",value=20, help = "Fragment mass tolerance (+/- around fragment m/z). (default: '20.0')"))
+        if int(Fragment_MT) <= 0:
+            st.error("Fragment mass tolerance must be a positive integer")
+
     with cols_[1]:
         Fragment_MT_unit= st.selectbox('Fragment mass tolerance unit',['ppm', 'Da'])
     
@@ -92,6 +113,8 @@ with cols[0]:
     preset = st.selectbox('Select the suitable preset',preset_list, help = " Set precursor and fragment adducts form presets (recommended).")
 with cols[1]:
     length= str(st.number_input("length",value=2, help = "Oligonucleotide maximum length.(default: '2')"))
+    if int(length) <= 0:
+        st.error("Length must be a positive integer greater than 0.")
 
 
 cols=st.columns(2)
@@ -103,15 +126,23 @@ with cols[1]:
 cols=st.columns(2)
 with cols[0]:
     Variable_max_per_peptide  = str(st.number_input("Variable_max_per_peptide",value=2, help="Maximum number of residues carrying a variable modification per candidate peptide.(default: '2')"))
+    if int(Variable_max_per_peptide) <= -1:
+        st.error("Variable_max_per_peptide must be a positive integer")
+
 with cols[1]:
     scoring  = st.selectbox('Select the scoring method',("slow", "fast"), help="Scoring algorithm used in prescoring (fast: total-loss only, slow: all losses). (default: 'slow') (valid: 'fast', 'slow')")
 
 ## take the exact name of file, create same file 
-mzML_file_name = mzML_file_path.split('\\')
-protocol_name = mzML_file_name[len(mzML_file_name)-1].replace(".mzML", "")
-result_path = str(result_dir)+"\\"+protocol_name+".idXML"
+#mzML_file_name = mzML_file_path.split('\\')
+#protocol_name = mzML_file_name[len(mzML_file_name)-1].replace(".mzML", "")
+#result_path = str(result_dir)+"\\"+protocol_name+".idXML"
+
+mzML_file_name = os.path.basename(mzML_file_path)
+protocol_name = os.path.splitext(mzML_file_name)[0]
+result_path = os.path.join(result_dir, protocol_name + ".idXML")
+
 #st.write(result_path)
-##################################### NuXL command ###########################
+##################################### NuXL command ############################
 
 result_dict = {}
 result_dict["success"] = False
